b0465b83b00a5369a1ab17da027900cefd088979
Asad777 <muhammadasad7.7.2007@gmail.com>
2024-09-29 12:50:31 +0500

    practise of read adn write

---
+++ readData.py
@@ -0,0 +1,27 @@
+# file = open("randomData.txt", 'r')
+# print(file.read())
+# file.close()
+
+
+# with  open("randomData.txt", 'r') as file:
+#     content = file.read()
+#     print(content)
+# print()
+# print(len(content))
+
+
+
+# file = open("randomData.txt" ,'r+')
+# print(file.read())
+# file.write("jhanzaib\nasad\nHunt\n")
+# file.close()
+
+
+# with open("randomData.txt" , 'r+') as w:
+#     w.read()
+#     w.write("asad\n")
+
+
+# with open("randomData.txt" ,'r') as x:
+#     content = x.read()
+#     print(content)
